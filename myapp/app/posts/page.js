c6fb3dd08ade8a06099bd7f4932540ff624e1641
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-01-15 20:14:07 -0500

    setup

---
+++ myapp/app/posts/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostReel = () => {
+  return (
+    <div>PostReel</div>
+  )
+}
+
+export default PostReel
---
+++ myapp/components/Homepage/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='px-4 py-8'>
+        <div className=''>
+            Hero
+        </div>
+    </section>
+  )
+}
+
+export default Hero
